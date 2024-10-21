8ad25833c77b27c579e4682f787fd76dc08684db
psmyrdek <przemek.smyrdek@gmail.com>
2024-10-21 14:50:04 +0200

    feat: err

--- src/pages/api/utils/error-handling.ts
+++ src/pages/api/utils/error-handling.ts
@@ -1,12 +1,6 @@
 export function errorHandler(error: unknown) {
   console.error('Error:', error);
-  return new Response(
-    JSON.stringify({
-      error: 'An error occurred while processing your request.',
-      message: error instanceof Error ? error.message : String(error),
-    }),
-    {
-      status: 500,
-    },
-  );
+  return new Response(JSON.stringify(error), {
+    status: 500,
+  });
 }
