508eaea16f86352f5746252e8c4f24c3c6c28615
psmyrdek <przemek.smyrdek@gmail.com>
2024-10-21 14:58:02 +0200

    feat: errors

--- src/pages/api/utils/error-handling.ts
+++ src/pages/api/utils/error-handling.ts
@@ -2,7 +2,7 @@ export function errorHandler(error: unknown) {
   const errorMessage =
     error instanceof Error ? error.message : 'An unknown error occurred';
 
-  return new Response(JSON.stringify({ error: errorMessage }), {
+  return new Response(JSON.stringify({ data: { error: errorMessage } }), {
     status: 500,
     headers: {
       'Content-Type': 'application/json',
