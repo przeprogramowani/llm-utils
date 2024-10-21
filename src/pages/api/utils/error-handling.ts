61ef8b05b5dd07248c0eca904440893c931eafac
psmyrdek <przemek.smyrdek@gmail.com>
2024-10-21 13:29:19 +0200

    feat: error handling

--- src/pages/api/utils/error-handling.ts
+++ src/pages/api/utils/error-handling.ts
@@ -3,6 +3,7 @@ export function errorHandler(error: unknown) {
   return new Response(
     JSON.stringify({
       error: 'An error occurred while processing your request.',
+      message: error instanceof Error ? error.message : String(error),
     }),
     {
       status: 500,
