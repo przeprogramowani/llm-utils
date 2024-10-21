b239aff01326f5603d4547ee8b2aa887b06511ed
psmyrdek <przemek.smyrdek@gmail.com>
2024-10-21 16:47:48 +0200

    chore: smaller input

--- src/features/storytelling/StorytellingForm.svelte
+++ src/features/storytelling/StorytellingForm.svelte
@@ -129,7 +129,7 @@
           <textarea
             id="input-text"
             bind:value={inputText}
-            rows="10"
+            rows="5"
             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             placeholder="Paste your article text here..."
           ></textarea>
